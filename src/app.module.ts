222c5dcf2ba484cd41e6cdefd312ed455e34a3ec
Khoi Tran <duykhoi96tn@gmail.com>
2023-10-02 09:03:39 +0700

    config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostgresDbConfig } from './config/database.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostgresDbConfig],
 })
 export class AppModule {}
